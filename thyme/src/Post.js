d4ce4d6e4a141462e13d4848022db59f8e1401d8
winniehcy <winniehcyeung@gmail.com>
2020-03-01 20:00:40 -0500

    card+post

---
+++ thyme/src/Post.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Post.css';
+
+function Post() {
+	return (
+		<div className="Post">
+      		<header className="Post-header">
+        		This is the Post header.
+      		</header>
+    
+    	</div>
+
+    	//    		<Container>
+    //     		<Row>
+			//     <Col>1 of 3</Col>
+			//     <Col>2 of 3</Col>
+			//     <Col>3 of 3</Col>
+			// </Row>
+			// </Container>
+	)
+}
+
+export default Post;
