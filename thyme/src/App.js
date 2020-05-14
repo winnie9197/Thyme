6494a5052c53e3f42cac624c52ac99caf2017a11
winniehcy <winniehcyeung@gmail.com>
2020-05-13 23:06:04 -0400

    folder structure

--- thyme/src/App.js
+++ thyme/src/App.js
@@ -1,70 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navigation from './components/Navigation/Navigation';
+import Landing from './components/Landing/Landing';
+// import logo from './logo.svg';
 import './App.css';
-import Post from './Post.js'
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-
-import Carousel from 'react-bootstrap/Carousel'
+// import Post from './Post.js';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+// import Carousel from 'react-bootstrap/Carousel';
 
 class App extends React.Component {
-
-  constructor() {
-    super();
-    
-    var info = [["1st Restaurant", "Restaurant or Post Description...", "./images/holder.jpg"],
-        ["2nd Restaurant", "Restaurant or Post Description...", "./images/holder2.jpg"],
-        ["3rd Restaurant", "Restaurant or Post Description...", "./images/holder3.jpg"]
-      ];
-
-    this.state = {postInfo: info};
-  }
-  
-  render() {
-    return (
-      <div className="App">
-        <Navbar activeKey="#home" variant="light">
-          <Navbar.Brand href="#home">
-          <img
-            src="./images/thyme-logo.png"
-            width="30"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"
-          />Thyme
-          </Navbar.Brand>
-          <Nav>
-            <Nav.Item>
-              <Nav.Link href="#home">What is Thyme?</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Get Started</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Thyme Pink</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link>Thyme Yellow</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar>
-
-        <Carousel>
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="./images/Yellow-landing-2.png"
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              It takes a meal to meet someone new.
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-
-
-        <Post postInfo={this.state.postInfo} />
-      </div>
+    render() {
+      return (
+        <div>
+          <Router>
+            <Navigation/>
+          </Router>
+          <Landing/>
+        </div>
     );
   }
 }
---
+++ thyme/src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+// import './Navigation.css';
+import React from 'react';
+
+class Navigation extends React.Component {
+	render() {
+		return (<div></div>)
+	}
+}
+
+export default Navigation;
